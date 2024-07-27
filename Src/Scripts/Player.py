2db7bdbe89cab2bdfdd1fb987f28bea7e9c607e7
Adit-Vij <aditvij2004@gmail.com>
2024-07-27 22:27:51 +0530

    Update Player.py
    Added jump dash, minor refactoring

--- Src/Scripts/Player.py
+++ Src/Scripts/Player.py
@@ -7,37 +7,58 @@ class Player(pygame.sprite.Sprite):
         self.image.fill((0,255,0))
         self.rect = self.image.get_rect()
         self.rect.x = x
-        self.rect.x = y
+        self.rect.y = y
         self.speed = 700
         self.gravity = 150
         self.velocity_y = 0
         self.is_on_ground = False
         self.jump_strength = -2000
-        self.is_jumping = False 
+        self.is_jumping = False
+
+        '''
+        ----Variables Advanced For Advanced Features----
+        '''
+        self.dash_speed = 4000
+        self.jump_dash_count = 0
 
     def update(self,keys,dt):
         move_speed = self.speed*dt
         jump_strength = self.jump_strength*dt
         gravity = self.gravity*dt
-
-        if keys[pygame.K_d]:
+        dash_speed = self.dash_speed*dt
+        #Movement Handling
+        '''
+        ----Basic Movement----
+        Left, Right, Jump
+        '''
+        if keys[pygame.K_d]:    #Move Right
             self.rect.x += move_speed
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a]:    #Move Left
             self.rect.x -= move_speed
-        if keys[pygame.K_SPACE] and self.is_on_ground:
+        if keys[pygame.K_SPACE] and self.is_on_ground:  #Jump Trigger
             self.is_jumping = True
-        if self.is_jumping:
+        if self.is_jumping:    #Jump
             self.velocity_y = jump_strength
             self.is_jumping = False
-
+            self.jump_dash_count += 2
+        '''
+        ----Advanced Movement----
+        Jump Dash
+        '''
+        if keys[pygame.K_q] and self.jump_dash_count>1:    #Jump Dash
+            self.rect.x += dash_speed
+            self.jump_dash_count -= 1
+            
         #Gravity
         self.velocity_y+= gravity
         self.rect.y += self.velocity_y
+        
 
         #Ground Check
         if self.rect.bottom > 600:
             self.rect.bottom = 600
             self.velocity_y = 0
+            #self.jump_dash_count = 2
             self.is_on_ground = True
         else:
             self.is_on_ground = False
