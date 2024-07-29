42660e0098753dae8de1cdc05b734b416d43c6b4
Adit-Vij <aditvij2004@gmail.com>
2024-07-29 23:20:05 +0530

    Update MainWindow.py

--- Src/Scripts/MainWindow.py
+++ Src/Scripts/MainWindow.py
@@ -1,52 +1,15 @@
-import sys
 import pygame
-from Player import Player
-from Obstacle import Obstacle
 
-#Initialize pygame
 pygame.init()
 
-#Window SetUp
-window_w= 800
-window_h= 600
+vector =pygame.math.Vector2
 
-window_size = (window_w,window_h)
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+ACCELERATION = 0.5
+FRICTION = -0.12
 
-window = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Sample")
-
-#Create a Player Imstance
-player = Player (window_w // 2, window_h // 2)
-
-#Create a Obstacle Instance
-obstacle1 = Obstacle(600,600,100,50,player)
-
-#Create sprite group
-all_sprites = pygame.sprite.Group()
-#all_sprites.add(player)
-all_sprites.add(obstacle1)
-all_sprites.add(player)
-
-#Clock
-clock = pygame.time.Clock()
-
-#Main Loop
-running = True
-
-while running:
-    dt = clock.tick(60)/1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys= pygame.key.get_pressed()
-    all_sprites.update(keys, dt)
-        
-    window.fill((0,0,0))
-    all_sprites.draw(window)
-
-    pygame.display.flip()
-        
-
-pygame.quit()
-sys.exit()
+FramesPerSecond = pygame.time.Clock()
+displayWindow = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Platformer")
